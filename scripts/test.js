60e87e8787dfc879026a36804ee83c3349bce4cb
Jordan White <jordan@volanosoftware.com>
2021-09-22 13:00:13 -0500

    misc

--- scripts/test.js
+++ scripts/test.js
@@ -1,4 +1,12 @@
-console.log("Eyyy I loaded")
+console.log("This code runs immediately when the file is loaded.");
+
+Hooks.on("init", function() {
+  console.log("This code runs once the Foundry VTT software begins its initialization workflow.");
+});
+
+Hooks.on("ready", function() {
+  console.log("This code runs once core initialization is ready and game data is available.");
+});
 
 // let chartTemplate = {
 //     type: 'pie',
