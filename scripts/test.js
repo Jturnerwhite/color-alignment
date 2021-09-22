435d4d3fd039007c1762839484c770f79ac47d02
Jordan White <jordan@volanosoftware.com>
2021-09-22 12:47:03 -0500

    file structure

---
+++ scripts/test.js
@@ -0,0 +1,30 @@
+console.log("Eyyy I loaded")
+
+// let chartTemplate = {
+//     type: 'pie',
+//     data: {
+//       labels: ["White", "Blue", "Black", "Red", "Green"],
+//       datasets: [{
+//         backgroundColor: ["#fbe792", "#017db2", "#681e73", "#e33b29", "#007c43"]
+//       }]
+//     },
+//     options: {
+// 		events: [],
+// 		plugins: {   
+// 			legend: {
+// 				display: false
+// 			},
+// 			tooltip: {
+// 				enabled: false
+// 			},
+//     		hover: {
+// 				mode: null
+// 			}
+// 		}
+// 	}
+// };
+
+// let instance = Object.assign({}, chartTemplate);
+// instance.data.datasets[0].data = [1, 3, 4, 1, 1]; 
+
+// new Chart(document.getElementById("pie-chart-1"), instance);
